9121b5d998567a8376826a1e32953e1c7b9e85e4
pwbolton77 <pwbolton77@gmail.com>
2021-09-06 14:05:59 -0700

    Added x64 compiler and linker settings to support Box2D.

--- SdlBox2dGameEng/Main.cpp
+++ SdlBox2dGameEng/Main.cpp
@@ -1,18 +1,85 @@
 /*This source code copyrighted by Lazy Foo' Productions (2004-2020)
 and may not be redistributed without written permission.*/
 
-// https://lazyfoo.net/tutorials/SDL/01_hello_SDL/windows/msvc2019/index.php
+// SDL Install: https://lazyfoo.net/tutorials/SDL/01_hello_SDL/windows/msvc2019/index.php
+// SDL OpenGL install: https://lazyfoo.net/tutorials/OpenGL/01_hello_opengl/index.php
+
+// Box2D tutorial 1: https://www.youtube.com/watch?v=QEzcr7AfZ0o&ab_channel=thecplusplusguy
 
 //Using SDL and standard IO
 #include <SDL.h>
 #include <stdio.h>
+//#include <GL/gl.h>
+//#include <GL/glu.h>
+#include <Box2D/Box2D.h>
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const float M2P = 20.0f;		// Meters to pixels
+const float P2M = 1.0f/M2P;	// Pixels to meters 
+b2World* world;
+
+b2Body* addRect(float x, float y, float w, float h, bool dyn = true)
+{
+	b2BodyDef bodydef;
+	bodydef.position.Set(x*P2M, y*P2M);
+	bodydef.type = (dyn) ? b2_dynamicBody : b2_staticBody;
+
+	b2Body* body = world->CreateBody(&bodydef);
+
+	b2PolygonShape shape;
+	shape.SetAsBox(P2M * w / 2, P2M * h / 2);
+
+	b2FixtureDef fixture_def;
+	fixture_def.shape = &shape;
+	fixture_def.density = 1.0;
+
+	body->CreateFixture(&fixture_def);
+	return body;
+}
+
+void drawSquare(b2Vec2* points, b2Vec2 center, float angle)
+{
+	// glColor3f(1.0, 1.0f, 1.0f);
+	// glPushMatrix();
+	//		glTranslatef(center.x*M2P, center.y*M2P, 0.0f);
+	//		glRotate(angle*180.0/M_PI, 0f, 0f, 1f);
+	//		glBegin(GL_QUADS);
+	//		for (int 1=0; i<4; ++i)
+	//			glVertex2f(points[i].x*M2P, points[i].y*M2P);
+
+	//		glEnd();
+	// glPopMatrix();
+}
+
+void display()
+{
+	// @@@ Clear display hstuff
+
+	b2Body* tmp = world->GetBodyList();
+
+	b2Vec2 points[4];
+
+	while (tmp != nullptr)
+	{
+		// Get points assuming a rectangle 
+		for (int i = 0; i < 4; ++i)
+			points[i] = ((b2PolygonShape*)tmp->GetFixtureList()->GetShape())->m_vertices[i];
+
+
+		drawSquare(points, tmp->GetWorldCenter(), tmp->GetAngle ());
+		tmp = tmp->GetNext();
+	}
+
+
+}
+
 
 int main(int argc, char* args[])
 {
+	world = new b2World(b2Vec2 (0.0, 9.81f));
+
 	//The window we'll be rendering to
 	SDL_Window* window = NULL;
 
